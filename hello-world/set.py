d8ba1d8afe3a1e9a05d7b18ce2dee00937075d47
vitor-dornela <vitor.dornela.m@gmail.com>
2023-12-19 23:15:09 -0300

    set

---
+++ hello-world/set.py
@@ -0,0 +1,6 @@
+holiday_items = {"present", "present", "present", "present", "present", "present", "present", "present", "present", "present", "present", "present", "present", "candy cane", "candy cane", "candy cane", "stocking", "stocking", "Christmas tree", "snow globe"}
+
+# A set just get the unique items from a list
+unique_holiday_items = set(holiday_items)
+print(unique_holiday_items) # {'candy cane', 'Christmas tree', 'snow globe', 'stocking', 'present'}
+

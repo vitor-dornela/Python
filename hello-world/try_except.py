c7e3a685c9238bc42bc4d15776bceac21286b883
vitor-dornela <vitor.dornela.m@gmail.com>
2023-12-21 00:21:21 -0300

    .

--- hello-world/try_except.py
+++ hello-world/try_except.py
@@ -3,3 +3,9 @@ try:
     print('this', 'won\'t', 'work', sep=8)
 except TypeError:
     print('TypeError: sep must be None or a string, not int')
+
+try:
+    price = int(input('Enter a new price: '))  # $451
+    print(price)
+except ValueError:
+    print('ErrorValue: Please enter an integer value.')
